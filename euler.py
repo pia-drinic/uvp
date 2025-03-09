0904a47e60f07a4da1e37c6e0d4ed084b7ff5a15
Pia Drinić <pd00327@student.uni-lj.si>
2025-03-09 23:21:42 +0100

    eulernal1

--- euler.py
+++ euler.py
@@ -1,7 +1,10 @@
 # 1. naloga
 def vsota_veckratnikov(n):
     vsota = 0
-    for i in range (0, n):
-        if i // 3 == 0 or i // 5 == 0:
+    for i in range(n):
+        if i % 3 == 0 or i % 5 == 0:
             vsota += i
-        return vsota
+    return vsota
+
+print(vsota_veckratnikov(10))
+print(vsota_veckratnikov(1000))
